10fd29de1f3e349c308b600db1d233fed68d03a2
Lucas Cisternas <l.cisternasopazo@gmail.com>
2021-11-24 16:57:02 -0300

    sidebar refactorizada

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Nav, Logo, Menu, Boton, Sidebar, Content, Close, SidebarItems, SidebarSettings, Items, Settings } from '../style/navbarStyle';
+import { Nav, Logo, Menu, Boton, Items, Settings } from '../style/navbarStyle';
+
+import Sidebar from './Sidebar';
 
 const Navbar = () => {
 
@@ -20,30 +22,7 @@ const Navbar = () => {
             </Menu>
 
             {open &&
-                <Sidebar>
-                    <Content>
-                        <Close>
-                            <button 
-                                onClick={() => setOpen(!open)}
-                            ><i className="far fa-times-circle" />
-                            </button>
-                        </Close>
-                        <SidebarItems>
-                            <a onClick={() => setOpen(!open)} href="#!">Home</a>
-                            <a onClick={() => setOpen(!open)} href="#!">About</a>
-                            <a onClick={() => setOpen(!open)} href="#!">Menu</a>
-                            <a onClick={() => setOpen(!open)} href="#!">Reviews</a>
-                        </SidebarItems>
-                        <SidebarSettings>
-                            <Boton
-
-                            ><i className="far fa-user-circle"></i></Boton>
-                            <Boton
-                            
-                            ><i className="fas fa-shopping-cart"></i></Boton>
-                        </SidebarSettings>
-                    </Content>
-                </Sidebar>
+                <Sidebar open={open} setOpen={setOpen} />
             }
 
             <Items>
--- src/style/navbarStyle.js
+++ src/style/navbarStyle.js
@@ -47,55 +47,6 @@ export const Boton = styled.button`
     padding: 0;
     margin: 0;
     cursor: pointer;
-`
-export const Sidebar = styled.div`
-    width: 100vw;
-    height: 100vh;
-    top: 0;
-    right: 0;
-    position: fixed;
-    background-color: white;
-`
-export const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-`
-export const Close = styled.div`
-    height: 10%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-end;
-    button {
-        margin-right: 2rem;
-        border: none;
-        font-size: 4rem;
-        background-color: transparent;
-        color: green;
-    }
-`
-export const SidebarItems = styled.div`
-    height: 60% ;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-evenly;
-    a {
-        text-decoration: none;
-        font-size: 3.5rem;
-        font-weight: 600;
-        color: #171c48;
-    }
-`
-export const SidebarSettings = styled.div`
-    height: 30%;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
-    i {
-        font-size: 3.5rem;
-        color: #171c48;
-    }
 ` 
 export const Items = styled.div`
     height: 100%;
