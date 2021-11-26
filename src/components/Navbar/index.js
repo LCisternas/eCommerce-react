18dee8a815c277f612bbbe3a2a91e5326c060090
Lucas Cisternas <l.cisternasopazo@gmail.com>
2021-11-26 16:38:29 -0300

    configuracion navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import {} from '../Navbar/style';
+
+import Sidebar from '../Sidebar';
+
+const Navbar = () => {
+
+    const [mobile, setMobile] = useState(false);
+
+    return (
+        <div>
+            navbar
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    );
+}
+ 
+export default Navbar;
